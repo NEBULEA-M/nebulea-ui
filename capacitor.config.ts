134a3b52e305dc5e45c51d03dfd5083b5f9a6de0
loidinhm31 <loidinh.git@gmail.com>
2023-12-25 13:07:00 +0700

    Init cross-platform ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nebulea-ui',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
