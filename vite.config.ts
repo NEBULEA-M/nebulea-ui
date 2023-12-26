d0d7f4b6133fe8f74b95391282b917e40b90b8f2
loidinhm31 <loidinh.git@gmail.com>
2023-12-26 11:28:43 +0700

    Set up path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,9 +9,12 @@ export default defineConfig({
     react(),
     legacy()
   ],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts"
   }
-})
+});
